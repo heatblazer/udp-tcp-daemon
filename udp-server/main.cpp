5313a82dfc158d530b99f8e2dfd0ab29fd50239a
heatblazer <ilianzapryanov@abv.bg>
2016-10-26 15:40:35 +0300

    main: added command line args -d or --daemon to start in daemon mode
    feature

--- udp-server/main.cpp
+++ udp-server/main.cpp
@@ -1,10 +1,42 @@
 #include "sapplication.h"
 #include "daemon.h"
 
+/// for now one option for
+/// daemonizing
+/// \brief getOpts
+/// \param str
+/// \return
+///
+static int getOpts(char* str)
+{
+    int opts = 0;
+    if ( (strcmp(str, "-d") == 0) ||
+         (strcmp(str, "--daemon") == 0)) {
+        opts = 1;
+    } else {
+        opts = 0;
+    }
+    return opts;
+}
+
 int main(int argc, char *argv[])
 {
-    iz::daemonize();
+    if (argc > 1) {
+        int opts = 0;
+        for(int i=0; i < argc; ++i) {
+           opts = getOpts(argv[i]);
+           switch(opts) {
+           case 1: // and only for now
+               iz::daemonize();
+               break;
+           case 0:
+           default:
+               break;
+           }
+        }
+    }
     iz::SApplication app(argc, argv);
     app.init();
     return app.exec();
 }
+
