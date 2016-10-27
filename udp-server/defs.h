c7a1ae25391d0431bd8a37cd09c974ff22c86399
heatblazer <ilianzapryanov@abv.bg>
2016-10-27 10:44:43 +0300

    defs: added

---
+++ udp-server/defs.h
@@ -0,0 +1,21 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+struct wav_hdr_t
+{
+    char riff_tag[4];
+    int  riff_len;
+    char wav_tag[4];
+    char fmt_tag[4];
+    int fmt_len;
+    short audio_format;
+    short num_channels;
+    int sample_rate;
+    int byte_rate;
+    short block_align;
+    short bits_per_sample;
+    char data_tag[4];
+    int data_len;
+};
+
+#endif // DEFS_H
