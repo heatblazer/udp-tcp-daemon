b9ca0da55bc544a1449ae2fd9f6b62ed3a1ee1f3
heatblazer <ilianzapryanov@abv.bg>
2016-11-01 14:50:00 +0200

    recorder-config: update

--- udp-server/utils/recorder-config.cpp
+++ udp-server/utils/recorder-config.cpp
@@ -7,6 +7,8 @@
 
 namespace iz {
 
+RecorderConfig* RecorderConfig::s_inst = nullptr;
+
 RecorderConfig::RecorderConfig(QObject *parent)
     : QObject(parent)
 {
@@ -15,7 +17,6 @@ RecorderConfig::RecorderConfig(QObject *parent)
 
 RecorderConfig::~RecorderConfig()
 {
-
 }
 
 bool RecorderConfig::loadFile(const QString &fname)
@@ -31,24 +32,80 @@ bool RecorderConfig::loadFile(const QString &fname)
                 // rework the logic here
                 if (reader.name() == "HotSwap") {
                     QXmlStreamAttributes attribs = reader.attributes();
-                    int dbg =0;
+                    for(int i=0; i < attribs.count(); ++i) {
+                        m_tags["HotSwap"].append(MPair<QString, QString>(
+                                                     attribs.at(i).name().toString(),
+                                                      attribs.at(i).value().toString()));
+                    }
                 } else if (reader.name() == "Channels") {
                     QXmlStreamAttributes attribs = reader.attributes();
-                    int dbg =0;
+                    for(int i=0; i < attribs.count(); ++i) {
+                        std::cout << attribs.at(i).name().toString().toStdString() << std::endl;
+                        std::cout << attribs.at(i).value().toString().toStdString() << std::endl;
+                        m_tags["Channels"].append(MPair<QString, QString>
+                                                  (attribs.at(i).name().toString(),
+                                                       attribs.at(i).value().toString()));
+                    }
+
                 } else if (reader.name() == "Wave") {
                     QXmlStreamAttributes attribs = reader.attributes();
-                    int dbg =0;
-                } else {
+                    for(int i=0; i < attribs.count(); ++i) {
+                        m_tags["Wave"].append(MPair<QString, QString>
+                                              (attribs.at(i).name().toString(),
+                                                       attribs.at(i).value().toString()));
 
+                    }
+
+                } else if (reader.name() == "Record"){
+                    QXmlStreamAttributes attribs = reader.attributes();
+                    for(int i=0; i < attribs.count(); ++i) {
+                        m_tags["Record"].append(MPair<QString, QString>
+                                                (attribs.at(i).name().toString(),
+                                                       attribs.at(i).value().toString()));
+                    }
+                } else if (reader.name() == "Network"){
+                    QXmlStreamAttributes attribs = reader.attributes();
+                    for(int i=0; i < attribs.count(); ++i) {
+                        m_tags["Network"].append(MPair<QString, QString>
+                                                (attribs.at(i).name().toString(),
+                                                       attribs.at(i).value().toString()));
+                    }
+                } else if (reader.name() == "Log") {
+                    QXmlStreamAttributes attribs = reader.attributes();
+                    for(int i=0; i < attribs.count(); ++i) {
+                        m_tags["Log"].append(MPair<QString, QString>
+                                                (attribs.at(i).name().toString(),
+                                                       attribs.at(i).value().toString()));
+                    }
+                } else {
+                    // dummy else for now
+                    // later will support
+                    // default config
                 }
             }
         }
     }
+    // safety check later !!!
+    return true;
 }
 
-QString &RecorderConfig::getAttribute(const QString &key)
+/// no check for now
+/// \brief RecorderConfig::getTagPairs
+/// \param tag - tag name
+/// \return  list of attributes
+///
+QList<MPair<QString, QString> > &RecorderConfig::getTagPairs(const QString &tag)
 {
-
+    return m_tags[tag];
 }
+
+RecorderConfig &RecorderConfig::Instance()
+{
+    if (s_inst == nullptr) {
+        s_inst = new RecorderConfig;
+    }
+    return *s_inst;
+}
+
 
 } // iz
--- udp-server/utils/recorder-config.h
+++ udp-server/utils/recorder-config.h
@@ -3,19 +3,30 @@
 
 #include <QFile>
 #include <QObject>
+#include <QHash>
+
+#include "types.h"
 
 namespace iz {
 
+// using in C++ as alternate to typedef, for now typedef
+typedef QList<MPair<QString, QString> >  PairList;
+
 class RecorderConfig : public QObject
 {
-public:
-    explicit RecorderConfig(QObject* parent=nullptr);
-    virtual ~RecorderConfig();
-    bool loadFile(const QString& fname);
-    QString& getAttribute(const QString& key);
 
+public:
+    static RecorderConfig& Instance();
+    bool loadFile(const QString& fname);
+    PairList& getTagPairs(const QString& tag);
 private:
+    explicit RecorderConfig(QObject* parent=nullptr);
+    ~RecorderConfig();
+
+    static RecorderConfig* s_inst;
     QString m_filename;
+    // tag name, attributes
+    QHash<QString, QList< MPair<QString, QString> > > m_tags;
 };
 
 }  // iz
