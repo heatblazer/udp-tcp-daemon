ab3578760cc967606801ad811d74c118daa18f02
heatblazer <ilianzapryanov@abv.bg>
2016-11-01 16:20:20 +0200

    recorder: more WIP

--- udp-server/recorder.cpp
+++ udp-server/recorder.cpp
@@ -1,5 +1,8 @@
 #include "recorder.h"
 
+// wav library //
+#include "wav-writer.h"
+
 namespace iz {
 
 Recorder::Recorder(QObject *parent)
@@ -11,6 +14,18 @@ Recorder::Recorder(QObject *parent)
 Recorder::~Recorder()
 {
     // cleanup()
+}
+
+void Recorder::record(const udp_data_t &data, uint32_t slot)
+{
+    if (slot > 31) {
+        return; // out of max chan index
+    }
+    uint32_t data_size = (sizeof(data) -
+            (sizeof(data.counter) + (sizeof(data.null_bytes)/sizeof(data.null_bytes[0])))
+            );
+
+    m_wavs[slot]->write((short*)data.data, data_size);
 }
 
 } // iz
--- udp-server/recorder.h
+++ udp-server/recorder.h
@@ -4,7 +4,12 @@
 // qt - paretn //
 #include <QObject>
 
+#include "types.h"
+#include "utils/recorder-config.h"
+
 namespace iz {
+
+class Wav;
 
 class Recorder : public QObject
 {
@@ -12,9 +17,19 @@ class Recorder : public QObject
 public:
     explicit Recorder(QObject* parent=nullptr);
     virtual ~Recorder(); // we may inherit it too
+    bool init(void);
+
+signals:
+    void recordedBytes(uint32_t bytes);
+
+public slots:
+    void record(const udp_data_t& data, uint32_t slot);
 
 private:
-
+    // 32 chans for now
+    // but will be configured outside
+    Wav* m_wavs[32];
+    RecorderConfig m_config;
 };
 
 } // iz
