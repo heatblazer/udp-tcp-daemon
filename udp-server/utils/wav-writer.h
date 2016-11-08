a862fde1c48c4591a70aa0ea420829d6ecdf45a0
heatblazer <ilianzapryanov@abv.bg>
2016-11-08 22:08:00 +0200

    wav-writer: removed wav header and moved it to types.h

--- udp-server/utils/wav-writer.h
+++ udp-server/utils/wav-writer.h
@@ -5,27 +5,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#include "types.h"
 #include "wav-writer-iface.h"
 
 /// the default wav header
 /// \brief The wav_hdr_t struct
 
-struct wav_hdr_t
-{
-    char riff_tag[4];
-    int  riff_len;
-    char wav_tag[4];
-    char fmt_tag[4];
-    int fmt_len;
-    short audio_format;
-    short num_channels;
-    int sample_rate;
-    int byte_rate;
-    short block_align;
-    short bits_per_sample;
-    char data_tag[4];
-    int data_len;
-};
 
 namespace iz {
 
