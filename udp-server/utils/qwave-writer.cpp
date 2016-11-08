99556b0f14814e7e3da646f928f16b21ce3942bd
heatblazer <ilianzapryanov@abv.bg>
2016-11-08 22:07:43 +0200

    qwavfile: started implementing qwav file

--- udp-server/utils/qwave-writer.cpp
+++ udp-server/utils/qwave-writer.cpp
@@ -3,4 +3,55 @@
 
 namespace iz {
 
+bool QWav::isOpened() const
+{
+    return m_wav.isOpen();
+}
+
+void *QWav::read()
+{
+    return nullptr;
+}
+
+void QWav::setupWave(int samples_per_sec, int bits_per_sec, int riff_len,
+                     int fmt_len,
+                     short audio_fmt,
+                     short chann_cnt)
+{
+    (void) samples_per_sec;
+    (void) bits_per_sec;
+    (void) riff_len;
+    (void) fmt_len;
+    (void) audio_fmt;
+    (void) chann_cnt;
+}
+
+int QWav::write(short data[], int len)
+{
+    (void) data;
+    (void) len;
+    return -1;
+}
+
+void QWav::close()
+{
+
+}
+
+bool QWav::open(const char *perms)
+{
+    (void) perms;
+    return false;
+}
+
+QWav::QWav(const QString &fname)
+{
+    (void) fname;
+}
+
+QWav::~QWav()
+{
+
+}
+
 } // iz
--- udp-server/utils/qwave-writer.h
+++ udp-server/utils/qwave-writer.h
@@ -4,12 +4,34 @@
 // qt stuff //
 #include <QFile>
 
+#include "types.h"
 #include "wav-writer-iface.h"
 
 namespace iz {
 /// newer concept for wave writer using QT
 /// \brief The QWave class
 ///
+class QWav : public WavIface
+{
+public:
+    explicit QWav(const QString& fname);
+    virtual ~QWav();
+    virtual bool open(const char* perms) = 0;
+    virtual void close() = 0;
+    virtual int write(short int data[], int len) = 0;
+    virtual void setupWave(int samples_per_sec, int bits_per_sec, int riff_len,
+                            int fmt_len, short audio_fmt,  short chann_cnt) = 0;
+    virtual void* read() = 0;
+    virtual bool isOpened() const = 0;
+private:
+    QString m_name;
+    QFile m_wav;
+    int   m_slot;
+    bool  m_setup;
+    wav_hdr_t m_header;
+
+};
+
 
 } // iz
 
